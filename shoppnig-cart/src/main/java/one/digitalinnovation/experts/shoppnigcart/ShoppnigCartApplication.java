791c0c646ea212763df7128b071ddded53988e86
sadysantos <sadysantos@hotmail.com.br>
2021-08-26 15:33:57 -0300

    Products
    Signed-off-by: sadysantos <sadysantos@hotmail.com.br>

---
+++ shoppnig-cart/src/main/java/one/digitalinnovation/experts/shoppnigcart/ShoppnigCartApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.experts.shoppnigcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppnigCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppnigCartApplication.class, args);
+	}
+
+}
---
+++ shoppnig-cart/src/test/java/one/digitalinnovation/experts/shoppnigcart/ShoppnigCartApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.experts.shoppnigcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppnigCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
